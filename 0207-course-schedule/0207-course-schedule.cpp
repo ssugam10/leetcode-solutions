29a9def8d0e7752e64bd2e56dec3abd48419e5bd
sugam <128995246+ssugam10@users.noreply.github.com>
2024-06-30 19:23:37 +0300

    Time: 11 ms (92.24%), Space: 17.6 MB (35.58%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<list<int>> graph; // Adjacency list representation of the graph
+    int v;  // Number of vertices
+    unordered_set<int> visited; // Visited set to keep track of visited nodes
+    vector<int> order; // To store the topological order
+
+    Solution() : v(0) {}
+
+    void add_edge(int a, int b, bool bidir = true) {
+        graph[a].push_back(b);
+        if (bidir) {
+            graph[b].push_back(a);
+        }
+    }
+
+    void topoBFS() {
+        vector<int> indegree(v, 0);
+        for (int i = 0; i < v; i++) {
+            for (auto neighbour : graph[i]) {
+                indegree[neighbour]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < v; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+                visited.insert(i);
+            }
+        }
+
+        while (!q.empty()) {
+            int node = q.front();
+            order.push_back(node);
+            q.pop();
+            for (auto neighbour : graph[node]) {
+                indegree[neighbour]--;
+                if (indegree[neighbour] == 0) {
+                    q.push(neighbour);
+                    visited.insert(neighbour);
+                }
+            }
+        }
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        v = numCourses;
+        graph.resize(v);
+
+        for (const auto& prereq : prerequisites) {
+            add_edge(prereq[1], prereq[0], false);
+        }
+
+        topoBFS();
+
+        if (v == visited.size()) return true;
+        return false;
+    }
+};
