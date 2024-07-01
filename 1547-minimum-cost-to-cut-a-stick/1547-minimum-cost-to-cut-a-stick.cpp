19b52b783d49c6623a6dbba23709f474309f5cfc
sugam <128995246+ssugam10@users.noreply.github.com>
2024-07-01 14:06:07 +0300

    Time: 28 ms (66.31%), Space: 34.7 MB (5.04%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -2,19 +2,40 @@ class Solution {
 public:
     vector<vector<int>> dp;
     
-    int f(int i, int j, vector<int> &cuts){
+//     int f(int i, int j, vector<int> &cuts){
         
-        if(i > j)   return 0;
+//         if(i > j)   return 0;
         
-        if(dp[i][j] != -1)  return dp[i][j];
+//         if(dp[i][j] != -1)  return dp[i][j];
         
-        int ans = INT_MAX;
-        int len = cuts[j+1] - cuts[i-1];
-        for(int k = i ; k <= j; k++){
-            ans = min(ans, f(i,k-1,cuts) + f(k+1,j,cuts));
+//         int ans = INT_MAX;
+//         int len = cuts[j+1] - cuts[i-1];
+//         for(int k = i ; k <= j; k++){
+//             ans = min(ans, f(i,k-1,cuts) + f(k+1,j,cuts));
+//         }
+        
+//         return dp[i][j] = ans + len;
+//     }
+    
+    int fbu(vector<int> &cuts, int n){
+        dp.clear();
+        dp.resize(205,vector<int>(205,0));
+        
+        for(int i = n; i>=1; i--){
+            for(int j = 1; j <= n; j++){
+                if(i > j)   continue;
+                
+                dp[i][j] = INT_MAX;
+                
+                for(int k = i ; k <= j; k++){
+                    dp[i][j] = min(dp[i][j], dp[i][k-1] + dp[k+1][j]);
+                }
+                
+                dp[i][j] += (cuts[j+1]-cuts[i-1]);
+            }
         }
         
-        return dp[i][j] = ans + len;
+        return dp[1][n];
     }
     
     int minCost(int n, vector<int>& cuts) {
@@ -30,11 +51,12 @@ public:
         
         cutting.push_back(n);
         
+        return fbu(cutting, cutting.size()-2);
         
         
-        dp.clear();
-        dp.resize(205,vector<int>(205,-1));
+        // dp.clear();
+        // dp.resize(205,vector<int>(205,-1));
         
-        return f(1,cutting.size() - 2,cutting);
+        //return f(1,cutting.size() - 2,cutting);
     }
 };
