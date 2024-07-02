794aaf8db958e910b14a6f0251190b61d84d29d3
sugam <128995246+ssugam10@users.noreply.github.com>
2024-07-02 08:29:29 +0300

    Time: 109 ms (75.35%), Space: 48.2 MB (6.33%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     vector<vector<int>> dp;
     
-    int f(vector<int> &nums, int i, int j){
+//     int f(vector<int> &nums, int i, int j){
         
-        if(i > j)   return 0;
+//         if(i > j)   return 0;
         
-        if(dp[i][j] != -1)  return dp[i][j];
+//         if(dp[i][j] != -1)  return dp[i][j];
         
-        int coins = -1;
-        for(int k = i ; k<=j; k++){
-            int gain = nums[i-1] * nums[k] * nums[j+1];
-            coins = max(coins,f(nums,i,k-1) + gain + f(nums,k+1,j));
+//         int coins = -1;
+//         for(int k = i ; k<=j; k++){
+//             int gain = nums[i-1] * nums[k] * nums[j+1];
+//             coins = max(coins,f(nums,i,k-1) + gain + f(nums,k+1,j));
+//         }
+        
+//         return dp[i][j] = coins;
+//     }
+    
+    int fbu(vector<int> &nums){
+        dp.clear();
+        dp.resize(305,vector<int>(305,0));
+        int n = nums.size() - 2;
+        
+        for(int len = 1; len <= n; len++){
+            for(int i = 1; i + len - 1 <= n; i++){
+                int j = i+len-1;    //last index
+                dp[i][j] = -1;
+                for(int k = i ; k<=j; k++){
+                    int gain = nums[i-1] * nums[k] * nums[j+1];
+                    dp[i][j] = max(dp[i][j] , dp[i][k-1] + gain + dp[k+1][j]);
+                }
+            }
         }
         
-        return dp[i][j] = coins;
+        return dp[1][n];
     }
     
     int maxCoins(vector<int>& nums) {
@@ -22,8 +41,8 @@ public:
         numbers.push_back(1);
         
         int n = nums.size();
-        dp.clear();
-        dp.resize(305,vector<int>(305,-1));
+        // dp.clear();
+        // dp.resize(305,vector<int>(305,-1));
         
         for(int i = 0; i<n; i++){
             numbers.push_back(nums[i]);
@@ -31,6 +50,8 @@ public:
         
         numbers.push_back(1);
         
-        return f(numbers,1,n);
+        // return f(numbers,1,n);
+        
+        return fbu(numbers);
     }
 };
