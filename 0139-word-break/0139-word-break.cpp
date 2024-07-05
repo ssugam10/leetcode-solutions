7bcc551b83e970689808e188794ac5c104d6778c
sugam <128995246+ssugam10@users.noreply.github.com>
2024-07-05 19:42:45 +0300

    Time: 22 ms (22.39%), Space: 19.1 MB (18.76%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int n;
+    vector<int> dp;
+    
+    bool isPresent(string temp, set<string> &s,string &A){
+        if(s.count(temp))   return true;
+        return false;
+    }
+
+    bool f(int i, string &A, set<string> &s){
+
+        if(i == n)  return true;
+
+        if(dp[i] != -1) return dp[i];
+
+        bool ans = false;
+        string temp = "";
+        for(int k = i; k<n; k++){
+            temp += A[k];
+            if(isPresent(temp,s,A)){
+                ans |= f(k+1,A,s);
+            }
+        }
+
+        return dp[i] = ans;
+    }
+
+    
+    
+    bool wordBreak(string A, vector<string>& wordDict) {
+        set<string> s;
+        for(int i = 0; i<wordDict.size(); i++){
+            s.insert(wordDict[i]);
+        }
+        n = A.size();
+        dp.assign(6505,-1);
+        return f(0,A,s);
+    }
+};
+
+
+
+
