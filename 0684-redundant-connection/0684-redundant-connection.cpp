836f18f492f58b4145d20a41ecdc25cb6f1981a2
sugam <128995246+ssugam10@users.noreply.github.com>
2024-07-22 10:40:50 +0300

    Time: 7 ms (48.65%), Space: 12.8 MB (21.88%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,52 +1,80 @@
 class Solution {
 public:
-    int find(int node, vector<int> &parent){
-        if(node == parent[node]){
-            return node;
+
+void dfs(int n,vector<vector<int>>&adj,vector<bool>&vis){
+    vis[n]=true;
+    for(auto i:adj[n]){
+        if(!vis[i]){
+            vis[i]=true;
+        dfs(i,adj,vis);
         }
-        
-        return parent[node] = find(parent[node],parent);
+    }
+}
+
+bool connected(vector<vector<int>>&adj,vector<bool>&vis,int n){
+    dfs(0,adj,vis);
+    
+    for(int i = 0; i<n; i++){
+        if(!vis[i]) return false;
     }
     
-    bool unionByRank(int a, int b, vector<int> &parent, vector<int> &rank){
-        int parentA = find(a,parent);
-        int parentB = find(b,parent);
-        
-        if(parentA == parentB){
-            return false;
-        }
-        
-        if(rank[parentA] > rank[parentB]){
-            parent[parentB] = parentA;
-        }
-        else if(rank[parentB] > rank[parentA]){
-            parent[parentA] = parentB;
-        }
-        else{
-            parent[parentB] = a;
-            rank[parentA]++;
-        }
-        
-        return true;
-    }
+    return true;
     
+    // int c=0;
+    // for(int i=0;i<n;i++){
+    //     if(vis[i]==0){
+    //     dfs(i,adj,vis);
+    //     c++;
+    // }
+    // }
+    // return c;
+
+}
+
+
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        
-        vector<int> parent(n+1,0);
-        vector<int> rank(n+1,1);
-        
-        for(int i = 1; i<=n; i++){
-            parent[i] = i;
+                int n=edges.size();
+
+vector<vector<int>>adj(n);
+        vector<int>v;
+        for(auto i:edges){
+            adj[i[0]-1].push_back(i[1]-1);
+            adj[i[1]-1].push_back(i[0]-1);
+
         }
+
+        for(int i=edges.size()-1;i>=0;i--){
+            int a=edges[i][0]-1;
+            int b=edges[i][1]-1;
+            auto it=find(adj[a].begin(),adj[a].end(),b);
+            adj[a].erase(it);
+
+            auto it1=find(adj[b].begin(),adj[b].end(),a);
+            adj[b].erase(it1);
+
+            vector<bool>vis(n,false);
+
+            // for(auto i:adj){
+            //     for(auto j:i)
+            //     cout<<j<<" ";
+            //     cout<<endl;
+            // }
+            // cout<<endl;
         
-        for(int i = 0; i<n; i++){
-            bool isPossible = unionByRank(edges[i][0], edges[i][1], parent, rank);
-            if(!isPossible){
-                return {edges[i][0], edges[i][1]};
+            bool ans=connected(adj,vis,n);
+
+            // cout<<ans<<endl;
+            if(ans==true ){
+                v.push_back(a+1);
+                v.push_back(b+1);
+                return v;
+}
+            else{
+            adj[a].push_back(b);
+            adj[b].push_back(a);
             }
         }
-        
-        return {};
+        return v;
+        // return {-1,-1};
     }
 };
