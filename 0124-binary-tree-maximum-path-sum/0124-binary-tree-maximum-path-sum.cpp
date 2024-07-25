01691dffab3d2a6b2563ee14da3e7a2f482328d1
sugam <128995246+ssugam10@users.noreply.github.com>
2024-07-26 01:39:01 +0530

    Time: 11 ms (96.94%), Space: 26.9 MB (13.57%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -11,29 +11,33 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, int> dp;   //stores the maximum path sum starting at a particular node from it till root
+    //unordered_map<TreeNode*, int> dp;   //stores the maximum path sum starting at a particular node from it till root
     int mx;
     
     int traverse(TreeNode *node){
         if(!node)   return 0;
-        return dp[node] = max(max({traverse(node->left), traverse(node->right)}) + node->val,0); 
+        int left = max(0,traverse(node->left));
+        int right = max(0,traverse(node->right));
+        
+        mx = max({mx, left + node->val, right + node->val, node->val, left + right + node->val});
+        
+        return node->val + max(left, right);
     }
     
-    void dfs(TreeNode *node){
-        if(!node)   return;
-        mx = max({mx, dp[node->left] + dp[node->right] + node->val});
-        dfs(node->left);
-        dfs(node->right);
-    }
-    
+    // void dfs(TreeNode *node){
+    //     if(!node)   return;
+    //     mx = max({mx, dp[node->left] + dp[node->right] + node->val});
+    //     dfs(node->left);
+    //     dfs(node->right);
+    // }
     
     int maxPathSum(TreeNode* root) {
-        dp[NULL] = 0;
+        //dp[NULL] = 0;
         mx = INT_MIN;
         
         traverse(root);
         
-        dfs(root);
+        //dfs(root);
         
         return mx;
     }
