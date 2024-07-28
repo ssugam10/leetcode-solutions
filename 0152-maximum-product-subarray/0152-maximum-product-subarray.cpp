4c2d5ff886deb85eb06c53c5c6cc14270c6ceac9
sugam <128995246+ssugam10@users.noreply.github.com>
2024-07-28 18:37:28 +0530

    Time: 3 ms (84.82%), Space: 16.5 MB (32.16%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,25 +1,52 @@
-#define ll long long int
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        
-        ll mx = INT_MIN;
-        ll prod = 1;
-        
-        for(int i = 0; i<n; i++){
-            prod *= nums[i];
-            mx = max(mx,prod);
-            if(prod == 0 || prod < INT_MIN)   prod = 1;
+    double maxi = INT_MIN;
+    double prod=1;
+
+        for(int i=0;i<nums.size();i++)
+        {
+          prod*=nums[i];
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
         }
-        
-        prod = 1;
-        for(int i = n - 1; i>=0; i--){
-            prod *= nums[i];
-            mx = max(mx,prod);
-            if(prod == 0 || prod < INT_MIN)   prod = 1;
+        prod=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+          prod*=nums[i];
+
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
         }
-        
-        return (int)mx;
+        return (int) maxi;
     }
 };
+
+// #define ll long long int
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         ll mx = INT_MIN;
+//         ll prod = 1;
+        
+//         for(int i = 0; i<n; i++){
+//             prod *= nums[i];
+//             mx = max(mx,prod);
+//             if(prod == 0 || prod < INT_MIN)   prod = 1;
+//         }
+        
+//         prod = 1;
+//         for(int i = n - 1; i>=0; i--){
+//             prod *= nums[i];
+//             mx = max(mx,prod);
+//             if(prod == 0 || prod < INT_MIN)   prod = 1;
+//         }
+        
+//         return (int)mx;
+//     }
+// };
+
