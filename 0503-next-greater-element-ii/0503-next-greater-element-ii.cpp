6c194ce1a2b413b97962a4c3f69ce5f81cebb8e3
sugam <128995246+ssugam10@users.noreply.github.com>
2024-08-05 19:54:23 +0530

    Time: 22 ms (61.26%), Space: 26.7 MB (49.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int original = nums.size();
-        
-        // Extend the array to simulate circular array
-        for (int i = 0; i < original - 1; i++) {
-            nums.push_back(nums[i]);
-        }
-        
         int n = nums.size();
+        
+        vector<int> nge(n);
         stack<int> st;
         
-        vector<int> nge(n, 0); // Vector to store next greater elements
-        nge[n - 1] = -1; // Last element in circular array has no greater element by default
-        
-        st.push(nums[n - 1]);
-        
-        for (int i = n - 2; i >= 0; i--) {
-            while (!st.empty() && nums[i] >= st.top()) {
-                st.pop();
-            }
+        for(int i = 2*n - 1; i>=0; i--){
+            while(!st.empty() && st.top() <= nums[i % n])   st.pop();
             
-            if (!st.empty()) {
-                nge[i] = st.top();
-            } else {
-                nge[i] = -1;
-            }
+            if(i < n)   nge[i] = (st.empty()) ? -1 : st.top();
             
-            st.push(nums[i]);
+            st.push(nums[i % n]);
         }
         
-        // Prepare the result vector containing only the next greater elements for the original array
-        vector<int> ans(original);
-        for (int i = 0; i < original; i++) {
-            ans[i] = nge[i];
-        }
-        
-        return ans;
+        return nge;
     }
 };
