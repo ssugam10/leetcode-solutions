f8328f320e92e5e547d4ebca573b14c2650a0fe7
sugam <128995246+ssugam10@users.noreply.github.com>
2024-08-11 17:44:37 +0530

    Time: 359 ms (29.32%), Space: 19.9 MB (39.59%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,64 +1,33 @@
 class Solution {
 public:
-    vector<list<int>> graph;
-
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int n = wordList.size();
-        int len = beginWord.length();
-        graph.clear();
-        graph.resize(n,list<int>());
-
-        queue<int> q;
-        vector<bool> visited(n,false);
-
-        for(int i = 0; i<n; i++){
-            int change = 0;
-            for(int k = 0; k<len;k++){
-                if(beginWord[k] != wordList[i][k])  change++;
-            }
-            if(change == 1){
-                q.push(i);
-                visited[i] = true;
-            }
-
-            for(int j = i+1; j<n ; j++){
-                int diff = 0;
-                for(int k = 0; k<len; k++){
-                    if(wordList[i][k] != wordList[j][k])    diff++;
-                }
-                if(diff == 1){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
-            }
-        }
-        q.push(-1);
-
-        int dist = 2;
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        
+        set<string> st(wordList.begin(), wordList.end());
+        st.erase(beginWord);
         
         while(!q.empty()){
-            int curr = q.front();
+            string curr = q.front().first;
+            int steps = q.front().second;
             q.pop();
-
-            if(curr == -1){
-                if(q.size() == 0)   return 0;
-                q.push(-1);
-                dist++;
-                continue;
-            }
-
-            if(wordList[curr] == endWord){
-                return dist;
-            }
-
-            for(auto neigh : graph[curr]){
-                if(!visited[neigh]){
-                    q.push(neigh);
-                    visited[neigh] = true;
+            
+            if(curr == endWord) return steps;
+            
+            for(int i = 0; i<curr.length(); i++){
+                
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    string newWord = curr;
+                    newWord[i] = ch;
+                    
+                    if(st.find(newWord) != st.end()){
+                        q.push({newWord,steps + 1});
+                        st.erase(newWord);
+                    }
                 }
             }
         }
-
+        
         return 0;
     }
 };
