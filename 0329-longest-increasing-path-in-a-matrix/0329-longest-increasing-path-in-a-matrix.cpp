168afe3fd04aefcb30b0a6f273b69bdcc41a4847
sugam <128995246+ssugam10@users.noreply.github.com>
2024-08-15 14:00:28 +0530

    Time: 36 ms (37.66%), Space: 19.5 MB (54.37%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,53 +1,45 @@
 class Solution {
 public:
+    int n;
+    int m;
+    
+    vector<pair<int,int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    
+    vector<vector<int>> dp;
+    
+    int f(int i, int j, vector<vector<int>> &grid){
+        
+        if(dp[i][j] != -1)  return dp[i][j];
+        
+        int maxPath = 1;
+        
+        for(int k = 0; k<4; k++){
+            int new_i = i + dirs[k].first;
+            int new_j = j + dirs[k].second;
+            
+            if(new_i >=0 && new_i < n && new_j >= 0 && new_j < m && grid[i][j] < grid[new_i][new_j]){
+                maxPath = max(maxPath, 1 + f(new_i, new_j, grid));
+            }
+        }
+        
+        return dp[i][j] = maxPath;
+    }
+    
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int m = matrix.size() , n = matrix[0].size() ;
-        vector<vector<int>> indegree(m , vector(n , 0)) ;
-        vector dir = {-1,0,1,0,-1} ;
-
-        for (int i = 0 ; i < m ; i++) {
-            for (int j = 0 ; j < n ; j++) {
-                for (int k = 0 ; k < 4 ; k++) {
-                    int next_i = i + dir[k] ; 
-                    int next_j = j + dir[k+1] ; 
-                    if (next_i >= 0 && next_j >= 0 && next_i < m && next_j < n 
-                        && matrix[next_i][next_j] > matrix[i][j]) {
-                        indegree[next_i][next_j]++ ; 
-                    }
-                }
+        n = matrix.size();
+        m = matrix[0].size();
+        
+        dp.resize(n,vector<int>(m,-1));
+        
+        int mx = 1;
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                mx = max(mx, f(i,j,matrix));
             }
         }
-
-        queue<pair<int,int>> Q ;
-
-        for (int i = 0 ; i < m ; i++) {
-            for (int j = 0 ; j < n ; j++) {
-                if (indegree[i][j] == 0) {
-                    Q.push({i,j}) ; 
-                }
-            }
-        }
-
-        int maxlen = 0 ;
-
-        while (!Q.empty()) {
-            int size = Q.size() ; 
-            for (int i = 0 ; i < size ; i++) {
-                auto [ x , y ] = Q.front() ; Q.pop() ; 
-                for (int k = 0 ; k < 4 ; k++) {
-                    int nx = x + dir[k] ; 
-                    int ny = y + dir[k+1] ; 
-                    if (nx >= 0 && ny >= 0 && nx < m && ny < n && matrix[nx][ny] > matrix[x][y]) {
-                        indegree[nx][ny]-- ; 
-                        if (indegree[nx][ny] == 0) {
-                            Q.push({nx,ny}) ; 
-                        }
-                    }
-                }
-            }
-            maxlen++ ; 
-        }
-
-        return maxlen ; 
+        
+        return mx;
+        
     }
 };
