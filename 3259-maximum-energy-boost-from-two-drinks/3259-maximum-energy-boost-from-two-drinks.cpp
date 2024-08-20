4942882ecd2664d530249a7474a4779900731687
sugam <128995246+ssugam10@users.noreply.github.com>
2024-08-20 18:27:02 +0530

    Time: 340 ms (25.92%), Space: 256.3 MB (25.28%) - LeetHub

--- 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.cpp
+++ 3259-maximum-energy-boost-from-two-drinks/3259-maximum-energy-boost-from-two-drinks.cpp
@@ -6,33 +6,16 @@ public:
     long long maxEnergyBoost(vector<int>& A, vector<int>& B) {
        int n = A.size();            
         
-//         vector<vector<ll>> dp(n+1,vector<ll>(n+1,0));
+        vector<vector<ll>> dp(n+1,vector<ll>(2,0));
         
-//         dp[n - 1][0] = A[n - 1];
-//         dp[n - 1][1] = B[n - 1];
-        
-        ll firstA = A[n - 1];
-        ll firstB = B[n - 1];
-        ll secondA = 0;
-        ll secondB = 0;
-        
-        ll currA = 0;
-        ll currB = 0;
+        dp[n - 1][0] = A[n - 1];
+        dp[n - 1][1] = B[n - 1];
         
         for(int i = n - 2; i>=0; i--){
-            // dp[i][0] = A[i] + max(dp[i+1][0], dp[i+2][1]);
-            // dp[i][1] = B[i] + max(dp[i+1][1], dp[i+2][0]);
-            currA = A[i] + max(firstA, secondB);
-            currB = B[i] + max(firstB, secondA);
-            
-            secondA = firstA;
-            secondB = firstB;
-            
-            firstA = currA;
-            firstB = currB;
-            
+            dp[i][0] = A[i] + max(dp[i+1][0], dp[i+2][1]);
+            dp[i][1] = B[i] + max(dp[i+1][1], dp[i+2][0]);
         }
         
-        return max(currA, currB);
+        return max(dp[0][0], dp[0][1]);
     }
 };
