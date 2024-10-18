ab7124827fcbf6e15222ca457cd6b1b83b99e455
sugam <128995246+ssugam10@users.noreply.github.com>
2024-10-18 14:19:20 +0530

    Time: 108 ms (13.36%), Space: 309.1 MB (9.05%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,41 +1,52 @@
 class Solution {
 public:
     int n;
+    vector<vector<int>> dp; // Memoization table
     
-    int f(int i, int mask, vector<int> &nums){
+    int countSubsets(int index, int currentMask, vector<int> &nums) {
+        // Base case: If we have considered all elements and the current mask is zero
+        if (index == n && currentMask == 0) return 1;
         
-        if(i == n && mask == 0) return 1;
-        
-        if(i == n)  return 0;
-        
-        int cnt = 0;
-        
-        int temp = mask;
-        
-        //consider nums[i]
-        
-        for(int j = 0; j<32; j++){
-            if((nums[i] & (1 << j) && (mask & (1 << j)))){
-                temp ^= (1 << j);
+        // If we considered all elements but mask is non-zero
+        if (index == n) return 0;
+
+        // Check if this state has already been computed
+        if (dp[index][currentMask] != -1) {
+            return dp[index][currentMask];
+        }
+
+        int count = 0;
+        int newMask = currentMask;
+
+        // Try including nums[index] in the subset
+        for (int bit = 0; bit < 32; ++bit) {
+            if ((nums[index] & (1 << bit)) && (currentMask & (1 << bit))) {
+                newMask ^= (1 << bit); // Toggle bits of the mask that are in nums[index]
             }
         }
-        
-        cnt = f(i+1,temp,nums);
-        
-        //dont consider nums[i]
-        cnt += f(i+1,mask,nums);
-        
-        return cnt;
+        // Recur with nums[index] included
+        count = countSubsets(index + 1, newMask, nums);
+
+        // Recur without including nums[index]
+        count += countSubsets(index + 1, currentMask, nums);
+
+        // Store the result in dp array before returning
+        return dp[index][currentMask] = count;
     }
-    
+
     int countMaxOrSubsets(vector<int>& nums) {
         n = nums.size();
-        
-        int mx = 0;
-        for(int i = 0; i<n; i++){
-            mx = mx | nums[i];
+        int maxOrValue = 0;
+
+        // Compute the OR of all elements in nums
+        for (int i = 0; i < n; ++i) {
+            maxOrValue |= nums[i];
         }
-        
-        return f(0,mx,nums);
+
+        // Initialize dp array with -1 indicating uncomputed states
+        dp.assign(n, vector<int>(maxOrValue + 1, -1));
+
+        // Start the recursion with all bits set in maxOrValue
+        return countSubsets(0, maxOrValue, nums);
     }
 };
