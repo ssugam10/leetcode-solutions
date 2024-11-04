a0c4601ccd8a135112f7ec8bfe28df77db06e6bb
sugam <128995246+ssugam10@users.noreply.github.com>
2024-11-04 18:31:49 +0530

    Time: 15 ms (85.47%), Space: 27.8 MB (83.84%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     string compressedString(string word) {
-        int n = word.size();
+        int n = word.length();
         string comp = "";
-        int qty = 0;
-        for(int i = 0; i<n; i++){
-            if(i == 0)  qty = 1;
-            else{
-                if(word[i] == word[i-1] && qty < 9){
-                    qty++;
-                }
-                else{
-                    comp += (to_string(qty) + word[i-1]);
-                    qty = 1;
-                }
+        
+        int cnt = 1;
+        char ch = word[0];
+        
+        for(int i = 1; i<n; i++){
+            
+            if(word[i] != ch || cnt == 9){
+                comp += (char)('0' + cnt);
+                comp += ch;
+                
+                ch = word[i];
+                cnt = 1;
             }
+            
+            else if(word[i] == ch)   cnt++;
         }
         
-        comp += (to_string(qty) + word[n-1]);
+        comp += (char)('0' + cnt);
+        comp += ch; 
         
         return comp;
-        
     }
 };
